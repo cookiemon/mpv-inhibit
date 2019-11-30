d116a30e92d073333d1b18fdce61bb859a9fe7c2
Cookiemon <rhannek@gmx.de>
2019-11-30 16:56:04 +0100

    Set thread name to 'mpv/inhibit'.

--- src/inhibit.cpp
+++ src/inhibit.cpp
@@ -4,8 +4,35 @@
 #include <mpv/client.h>
 
 #include <string_view>
+#if defined(WIN32)
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#define os_text(x) (L##x)
+using os_str_type = const wchar_t*;
+#elif defined(__linux)
+#include <pthread.h>
+using os_str_type = const char*;
+constexpr auto os_text(os_str_type x) -> os_str_type {
+  return x;
+}
+#else
+#include <pthread_np.h>
+#define text(x) (x)
+using os_str_type = const char*;
+constexpr auto os_text(os_str_type x) -> os_str_type {
+  return x;
+}
+#endif
 
-using namespace std::string_view_literals;
+static void set_thread_name(os_str_type name) {
+#if defined(WIN32)
+  SetThreadDescription(GetCurrentThread(), name);
+#elif defined(__linux)
+  pthread_setname_np(pthread_self(), name);
+#else
+  pthread_set_name_np(pthread_self(), name);
+#endif
+}
 
 static void unpause(org_freedesktop_ScreenSaver& screen_saver,
                     uint32_t& cookie) {
@@ -29,6 +56,8 @@ static void pause(org_freedesktop_ScreenSaver& screen_saver, uint32_t& cookie) {
 
 extern "C" {
 int mpv_open_cplugin(mpv_handle* handle) {
+  set_thread_name(os_text("mpv/inhibit"));
+
   org_freedesktop_ScreenSaver screen_saver;
 
   uint32_t cookie = 0;
