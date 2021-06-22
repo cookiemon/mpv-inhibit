1d82bf1acaabfca7d7d74d9559b69f3f399c79e4
Cookiemon <rhannek@gmx.de>
2021-06-22 02:39:02 +0200

    Use the "more modern" mpv_observe_property_change api.

--- src/inhibit.cpp
+++ src/inhibit.cpp
@@ -3,7 +3,7 @@
 #include <fmt/format.h>
 #include <mpv/client.h>
 
-#include <pthread.h>
+#include <cstdint>
 #include <string_view>
 #if defined(WIN32)
 #define WIN32_LEAN_AND_MEAN
@@ -35,13 +35,13 @@ static void set_thread_name(os_str_type name) {
 #endif
 }
 
+constexpr uint64_t L33T = 1337;
+
 static void unpause(org_freedesktop_ScreenSaver& screen_saver,
                     uint32_t& cookie) {
   // New state: unpaused, deactivate screensaver
   if (cookie == 0) {
     cookie = screen_saver.Inhibit("mpv", "playing movie");
-  } else {
-    fmt::print("Double inhibit request detected.\n");
   }
 }
 
@@ -50,20 +50,23 @@ static void pause(org_freedesktop_ScreenSaver& screen_saver, uint32_t& cookie) {
   if (cookie != 0) {
     screen_saver.UnInhibit(cookie);
     cookie = 0;
-  } else {
-    fmt::print("Double uninhibit request detected.\n");
   }
 }
 
 extern "C" {
-int mpv_open_cplugin(mpv_handle* handle) {
+auto mpv_open_cplugin(mpv_handle* handle) -> int {
   set_thread_name(os_text("mpv/inhibit"));
 
   org_freedesktop_ScreenSaver screen_saver;
 
   uint32_t cookie = 0;
 
-  unpause(screen_saver, cookie);
+  auto res = mpv_observe_property(handle, L33T, "pause", MPV_FORMAT_FLAG);
+  if (res < 0) {
+    fmt::print("Cannot register property observer. Error: {}",
+               mpv_error_string(res));
+    return -1;
+  }
 
   // Enter event loop
   //////////////////////////////////////////////////////////////////////
@@ -73,12 +76,20 @@ int mpv_open_cplugin(mpv_handle* handle) {
     case MPV_EVENT_SHUTDOWN:
       return 0;
 
-    case MPV_EVENT_PAUSE:
-      pause(screen_saver, cookie);
-      break;
-    case MPV_EVENT_UNPAUSE:
-      unpause(screen_saver, cookie);
-      break;
+    case MPV_EVENT_PROPERTY_CHANGE:
+      // Should always be set but check just in case.
+      if (evt->reply_userdata == L33T) {
+        auto* data = static_cast<mpv_event_property*>(evt->data);
+        // Should never be somethimg else but check just in case.
+        if (data->format == MPV_FORMAT_FLAG && data->data != nullptr) {
+          int flag = *static_cast<int*>(data->data);
+          if (flag != 0) {
+            unpause(screen_saver, cookie);
+          } else {
+            pause(screen_saver, cookie);
+          }
+        }
+      }
 
     default:
       break;
